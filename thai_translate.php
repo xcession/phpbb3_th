3763fe1a14569c9a7f5d3729f0a7095cbfa2fd92
xcession <xcession@archeee.localdomain>
2008-10-29 03:24:25 +0700

    Minor fix thai_translate.php

--- thai_translate.php
+++ thai_translate.php
@@ -5,5 +5,5 @@ if (!defined('IN_PHPBB'))
 {
 	exit;
 }
-$thai = 'ภาษาไทยโดย <a href="http://www.mindphp.com" target="_bank">Mindphp.com</a> &amp; <a href="http://www.mindphp.com" target="_bank">MphpBBThailand.com</a>';
+$thai = 'Thai Language Pack by <a href="http://www.mindphp.com" target="_bank">Mindphp.com</a> &amp; <a href="http://www.mindphp.com" target="_bank">MphpBBThailand.com</a>';
 ?>
