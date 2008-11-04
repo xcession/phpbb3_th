c936435d59babd345850ffc98e451155e3d50122
xcession <xcession@gmail.com>
2008-11-05 06:45:06 +0700

    Remove unused file.

--- thai_translate.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Modified by xcession
-
-if (!defined('IN_PHPBB'))
-{
-	exit;
-}
-$thai = 'Thai Language Pack by <a href="http://www.mindphp.com" target="_bank">Mindphp.com</a> &amp; <a href="http://www.mindphp.com" target="_bank">MphpBBThailand.com</a>';
-?>
